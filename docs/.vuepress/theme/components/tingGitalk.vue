7574fb573c788b162d859a94c8b32c6b0c2e1594
XJyating <924471303@qq.com>
2020-04-01 17:32:16 +0800

    修改一下gitalk和live2d

--- docs/.vuepress/theme/components/tingGitalk.vue
+++ docs/.vuepress/theme/components/tingGitalk.vue
@@ -14,17 +14,10 @@
         },
         mounted() {
             if (this.$site.themeConfig.gitalk) {
-                var gitalk = {
-                    clientID: this.$site.themeConfig.gitalk.clientID,
-                    clientSecret: this.$site.themeConfig.gitalk.clientSecret,
-                    repo: `${this.$site.themeConfig.gitalk.githubName}.github.io`,
-                    owner: this.$site.themeConfig.gitalk.githubName,
-                    admin: [`${this.$site.themeConfig.gitalk.githubName}`],
-                    id: location.pathname, // Ensure uniqueness and length less than 50
-                    distractionFreeMode: false // Facebook-like distraction free mode
-                }
-                var gitalk = new Gitalk(gitalk)
-                gitalk.render('gitalk-container')
+                var gitalk = this.$site.themeConfig.gitalk;
+                gitalk.id=location.pathname;
+                var newgitalk = new Gitalk(gitalk)
+                newgitalk.render('gitalk-container')
             }else {
                 this.ifshow=false;
             }
--- docs/.vuepress/theme/public/js/message.js
+++ docs/.vuepress/theme/public/js/message.js
@@ -38,7 +38,7 @@ var result = {
 
 function renderTip(template, context) {
     var tokenReg = /(\\)?\{([^\{\}\\]+)(\\)?\}/g;
-    return template.replace(tokenReg, function (word, slash1, token, slash2) {
+    return template.replace(tokenReg, function(word, slash1, token, slash2) {
         if (slash1 || slash2) {
             return word.replace('\\', '');
         }
@@ -54,24 +54,24 @@ function renderTip(template, context) {
     });
 }
 
-String.prototype.renderTip = function (context) {
+String.prototype.renderTip = function(context) {
     return renderTip(this, context);
 };
 
 var re = /x/;
 console.log(re);
-re.toString = function () {
+re.toString = function() {
     showMessage('哈哈，你打开了控制台，是想要看看我的秘密吗？', 5000);
     return '';
 };
 
-$(document).on('copy', function () {
+$(document).on('copy', function() {
     showMessage('你都复制了些什么呀，转载要记得加上出处哦~~', 5000);
 });
 
 function initTips() {
-    $.each(result.mouseover, function (index, tips) {
-        $(tips.selector).mouseover(function () {
+    $.each(result.mouseover, function(index, tips) {
+        $(tips.selector).mouseover(function() {
             var text = tips.text;
             if (Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1) - 1];
             text = text.renderTip({
@@ -80,8 +80,8 @@ function initTips() {
             showMessage(text, 1000);
         });
     });
-    $.each(result.click, function (index, tips) {
-        $(tips.selector).click(function () {
+    $.each(result.click, function(index, tips) {
+        $(tips.selector).click(function() {
             var text = tips.text;
             if (Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1) - 1];
             text = text.renderTip({
@@ -93,7 +93,7 @@ function initTips() {
 }
 initTips();
 
-(function () {
+(function() {
     var text;
     if (document.referrer !== '') {
         var referrer = document.createElement('a');
@@ -108,26 +108,26 @@ initTips();
             text = '嗨！ 来自 谷歌搜索 的朋友！<br>欢迎访问<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
         }
     } else {
-            var now = (new Date()).getHours();
-            if (now > 23 || now <= 5) {
-                text = '你是夜猫子呀？这么晚还不睡觉，明天起的来嘛？';
-            } else if (now > 5 && now <= 7) {
-                text = '早上好！一日之计在于晨，美好的一天就要开始了！';
-            } else if (now > 7 && now <= 11) {
-                text = '上午好！工作顺利嘛，不要久坐，多起来走动走动哦！';
-            } else if (now > 11 && now <= 14) {
-                text = '中午了，工作了一个上午，现在是午餐时间！';
-            } else if (now > 14 && now <= 17) {
-                text = '午后很容易犯困呢，今天的运动目标完成了吗？';
-            } else if (now > 17 && now <= 19) {
-                text = '傍晚了！窗外夕阳的景色很美丽呢，最美不过夕阳红~~';
-            } else if (now > 19 && now <= 21) {
-                text = '晚上好，今天过得怎么样？';
-            } else if (now > 21 && now <= 23) {
-                text = '已经这么晚了呀，早点休息吧，晚安~~';
-            } else {
-                text = '嗨~ 快来逗我玩吧！';
-            }
+        var now = (new Date()).getHours();
+        if (now > 23 || now <= 5) {
+            text = '你是夜猫子呀？这么晚还不睡觉，明天起的来嘛？';
+        } else if (now > 5 && now <= 7) {
+            text = '早上好！一日之计在于晨，美好的一天就要开始了！';
+        } else if (now > 7 && now <= 11) {
+            text = '上午好！工作顺利嘛，不要久坐，多起来走动走动哦！';
+        } else if (now > 11 && now <= 14) {
+            text = '中午了，工作了一个上午，现在是午餐时间！';
+        } else if (now > 14 && now <= 17) {
+            text = '午后很容易犯困呢，今天的运动目标完成了吗？';
+        } else if (now > 17 && now <= 19) {
+            text = '傍晚了！窗外夕阳的景色很美丽呢，最美不过夕阳红~~';
+        } else if (now > 19 && now <= 21) {
+            text = '晚上好，今天过得怎么样？';
+        } else if (now > 21 && now <= 23) {
+            text = '已经这么晚了呀，早点休息吧，晚安~~';
+        } else {
+            text = '嗨~ 快来逗我玩吧！';
+        }
     }
     showMessage(text, 12000);
 })();
@@ -135,7 +135,7 @@ initTips();
 window.setInterval(showHitokoto, 30000);
 
 function showHitokoto() {
-    $.getJSON('https://v1.hitokoto.cn/', function (result) {
+    $.getJSON('https://v1.hitokoto.cn/', function(result) {
         showMessage(result.hitokoto, 5000);
     });
 }
@@ -155,9 +155,6 @@ function hideMessage(timeout) {
 }
 
 function initLive2d() {
-    $('.hide-button').on('click', () => {
-        $('#live2d').fadeToggle();
-    })
     $('#landlord').hover(() => {
         $('.hide-button').fadeIn(600);
     }, () => {
